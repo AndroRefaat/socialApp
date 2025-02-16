427fac158e183f892231e0417aab441485b9a49a
AndroRefaat <androrefaat44@gmail.com>
2025-02-17 00:09:48 +0200

    remove upload

--- src/utils/file uploading/multerUpload.js
+++ src/utils/file uploading/multerUpload.js
@@ -1,26 +1,26 @@
 
-import multer, { diskStorage } from "multer";
-import { nanoid } from "nanoid";
+// import multer, { diskStorage } from "multer";
+// import { nanoid } from "nanoid";
 
-export const fileValidation = {
-    images: ["image/jpeg", "image/jpg", "image/png"],
+// export const fileValidation = {
+//     images: ["image/jpeg", "image/jpg", "image/png"],
 
-}
+// }
 
 
 
-export const upload = (fileType, folder) => {
-    const storage = diskStorage({
-        destination: folder,
-        filename: (req, file, cb) => {
-            cb(null, nanoid() + "__" + file.originalname)
-        }
-    })
-    const fileFilter = (req, file, cb) => {
-        if (!fileType.includes(file.mimetype)) return cb(new Error("Invalid file type"), false);
-        return cb(null, true)
-    }
+// export const upload = (fileType, folder) => {
+//     const storage = diskStorage({
+//         destination: folder,
+//         filename: (req, file, cb) => {
+//             cb(null, nanoid() + "__" + file.originalname)
+//         }
+//     })
+//     const fileFilter = (req, file, cb) => {
+//         if (!fileType.includes(file.mimetype)) return cb(new Error("Invalid file type"), false);
+//         return cb(null, true)
+//     }
 
-    const multerUpload = multer({ storage, fileFilter });
-    return multerUpload
-}
+//     const multerUpload = multer({ storage, fileFilter });
+//     return multerUpload
+// }
